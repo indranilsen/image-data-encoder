b420e5c6dfe1d0b2fbbba42e95d559332c581a4d
Indranil Sen <indranilsen010@gmail.com>
2018-03-04 13:12:39 -0500

    Added end of message symbol and fixed char * scoping issues

--- Encoder.cpp
+++ Encoder.cpp
@@ -4,6 +4,9 @@
 #include <fstream>
 #include "Encoder.h"
 
+const int END_SYMBOL_SIZE = 4;
+const char* END_SYMBOL = "@$$@";
+
 std::string read_message_from_file(const char* filename) {
     std::string message, line;
     std::ifstream file(filename);
@@ -19,9 +22,24 @@ std::string read_message_from_file(const char* filename) {
     return message;
 }
 
+const char* append_suffix(const char* str, const char* suffix) {
+    std::string s(str);
+    s = s.substr(0, s.find(".png")) + suffix + s.substr(s.find(".png"), s.length());
+    const char *val = s.c_str();
+    char *ret_val = new char[s.length()];
+    strcpy(ret_val, val);
+    return ret_val;
+}
+
+void append_end_symbol(std::bitset<8>* bytes, long len) {
+    for (int i = 0; i < END_SYMBOL_SIZE; i++) {
+        bytes[len + i] = std::bitset<8>(END_SYMBOL[i]);
+    }
+}
+
 Encoder::Encoder(PNG_Img *img) {
     orig_img = img;
-    new_img = new PNG_Img("../encoded.png");
+    new_img = new PNG_Img(append_suffix(orig_img->get_filename(), "_encoded"));
 }
 
 Encoder::~Encoder() {
@@ -36,22 +54,26 @@ Encoder::~Encoder() {
 
 void Encoder::set_message(std::string str) {
     message = str;
-    bytes = new std::bitset<8> [str.length()];
+    bytes = new std::bitset<8> [str.length() + END_SYMBOL_SIZE];
 
     for (int i = 0; i < str.length(); i++) {
         bytes[i] = std::bitset<8>(str.c_str()[i]);
     }
+
+    append_end_symbol(bytes, str.length());
 }
 
 void Encoder::set_message_from_file(const char* filename) {
     std::string str = read_message_from_file(filename);
 
     message = str;
-    bytes = new std::bitset<8> [str.length()];
+    bytes = new std::bitset<8> [str.length() + END_SYMBOL_SIZE];
 
     for (int i = 0; i < str.length(); i++) {
         bytes[i] = std::bitset<8>(str.c_str()[i]);
     }
+
+    append_end_symbol(bytes, str.length());
 }
 
 int Encoder::encode_message() {
